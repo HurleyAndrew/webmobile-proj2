d47fa36ff1e7d8c6501e96023000c749a782d191
Andrew Hurley <andrewhurley@Andrews-MacBook-Pro.local>
2017-04-05 18:13:53 -0400

    more stuff

--- nav.js
+++ nav.js
@@ -1,20 +1,4 @@
 document.addEventListener("DOMContentLoaded",function(){
-	var navdata = `<div class="nav"> 
-			<a href="index.html" class="nav_item"><div>
-				Home
-			</div></a>
-			<a href="http.html" class="nav_item"><div>
-				HTTP
-			</div></a>
-			<a href="pioneers.html" class="nav_item"><div>
-				Pioneers
-			</div></a>
-			<a href="terminology.html" class="nav_item"><div>
-				Terminology
-			</div></a>
-			<a href="references.html" class="nav_item"><div>
-				References
-			</div></a>
-		</div>`;
+	var navdata = '<div class="nav"> <a href="index.html" class="nav_item"><div>Home </div></a> <a href="http.html" class="nav_item"><div>HTTP </div></a>  <a href="pioneers.html" class="nav_item"><div>Pioneers </div></a> <a href="terminology.html" class="nav_item"><div>Terminology </div></a> <a href="references.html" class="nav_item"><div>References </div></a> </div>';
 	document.getElementById("mainnav").innerHTML+=navdata;
 })
